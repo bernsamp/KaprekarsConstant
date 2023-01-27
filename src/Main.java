322ed22942791c6eb74e000396d5cbfe27711e84
Bernardo Sampaio <119073813+bernsamp@users.noreply.github.com>
2023-01-27 21:28:35 +0000

    Added more features such as console ouputs

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,28 @@
 import java.util.*;
 
 public class Main {
-    public static int KaprekarsConstant(int num) {
-        int diff = 0, count = 0;
-        while (diff != 6174) {
-            String s1 = String.valueOf(num);
+
+    /* More info: https://en.wikipedia.org/wiki/6174_(number)
+
+    6174 is known as Kaprekar's constant[1][2][3] after the Indian mathematician D. R. Kaprekar. This number is renowned for the following rule:
+
+    Take any four-digit number, using at least two different digits (leading zeros are allowed).
+    Arrange the digits in descending and then in ascending order to get two four-digit numbers, adding leading zeros if necessary.
+    Subtract the smaller number from the bigger number.
+    Go back to step 2 and repeat.
+
+     */
+    public static int KaprekarsConstant(int number) {
+
+        final int kaprekarsConstant = 6174;
+
+        int difference = 0;
+
+        int count = 0;
+
+        while (difference != kaprekarsConstant) {
+
+            String s1 = String.valueOf(number);
 
             char[] ch1 = new char[s1.length()];
             for (int i = 0; i < ch1.length; i++) {
@@ -13,34 +31,43 @@ public class Main {
             Arrays.sort(ch1);
             String s2 = new String(ch1);
 
-            String s3 = "";
-            for (int j = s2.length() - 1; j >= 0; j--) {
-                s3 += s2.charAt(j);
+            StringBuilder s3 = new StringBuilder();
+            for (int j = s2.length() -1; j >= 0; j--) {
+                s3.append(s2.charAt(j));
             }
 
             int a = Integer.parseInt(s2);
-            int b = Integer.parseInt(s3);
-            if (a > b) {
-                diff = a - b;
-            } else if (b > a) {
-                diff = b - a;
-            } else {
-                System.out.println("Result can't be reached");
-                break;
+            int b = Integer.parseInt(s3.toString());
 
+            System.out.println("Operand in descending order: " + b);
+            System.out.println("Operand in ascending order: " + a);
+
+            if (a > b) {
+                difference = a - b;
+                System.out.println("Difference is: " + difference + "\n");
+            } else if (b > a) {
+                difference = b - a;
+                System.out.println("Difference is: " + difference + "\n");
+            } else {
+                System.out.println("Kaprekar's constant can't be reached");
+                break;
             }
             count++;
-            num = diff;
+            number = difference;
 
         }
         return count;
-
     }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.print(KaprekarsConstant(sc.nextInt()));
+
+        System.out.println("Enter a four-digit number, using at least two different digits (leading zeros are allowed):");
+
+        int userInput = sc.nextInt();
+
+        System.out.print("The number of operations needed to reach the Kaprekar's Constant is: " + KaprekarsConstant(userInput));
     }
 
 }
